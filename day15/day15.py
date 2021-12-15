e095331203084816292a48e69d29fe7c91dc0ebd
Martin Sträng <martin.strang@alelion.com>
2021-12-15 21:39:52 +0100

    Sådärja

--- day15/day15.py
+++ day15/day15.py
@@ -1,33 +1,7 @@
-#import heapq
 import math
-from collections import namedtuple
 from itertools import chain
 from dataclasses import dataclass
 from queue import PriorityQueue
-
-
-test = """
-1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581
-"""
-
-#data = test
-with open('input', 'r') as f:
-    data = f.read()
-
-risks = [[int(c) for c in line] for line in data.strip().split()]
-xmax = len(risks)
-ymax = len(risks[0])
-print(f'Grid: {xmax}x{ymax}')
-Pos = namedtuple('Pos', ['x', 'y'])
 
 
 @dataclass(order=True)
@@ -37,20 +11,6 @@ class Node:
     risk: int
     prev: object
     visited: bool
-
-
-def x_iter(grid):
-    for x in range(xmax):
-        yield x
-
-def y_iter(grid):
-    for y in range(ymax):
-        yield y
-
-def xy_iter(grid):
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            yield x, y
 
 
 def neighbors(node, nodes):
@@ -67,46 +27,67 @@ def neighbors(node, nodes):
     return n
 
 
-nodes = [[Node(total=math.inf, risk=risks[y][x], pos=Pos(x, y), prev=None, visited=False) for x in x_iter(risks)] for y in y_iter(risks)]
+def dijkstra_eller_nåt(risk_grid):
+    xmax = len(risk_grid)
+    ymax = len(risk_grid[0])
+    nodes = [[Node(math.inf, (x, y), risk_grid[y][x], None, False) for x in range(xmax)] for y in range(ymax)]
+
+    Q = PriorityQueue(ymax*xmax)
+
+    nodes[0][0].total = 0
+    nodes[0][0].risk = 0
+
+    Q.put(nodes[0][0])
+
+    while not Q.empty():
+        cur = Q.get()
+
+        for n in neighbors(cur, nodes):
+            if not n.visited and cur.total + n.risk < n.total:
+                n.total = cur.total + n.risk
+                n.prev = cur
+                Q.put(n)
+
+    n = nodes[ymax-1][xmax-1]
+    return n.total
 
 
-n = nodes[ymax-1][xmax-1]
-print(f'{n.pos}, {n.risk} {n.total}')
+def wow_such_risk(risks):
+    def incwrap(v, a):
+        v += a
+        return v if v <= 9 else v - 9
 
-Q = PriorityQueue(ymax*xmax)
-#unvisited = list(chain(*nodes))
+    more_risks = []
+    for cols in risks:
+        more_risks.append(list(chain(*[list(map(lambda v: incwrap(v, a), cols)) for a in range(5)])))
 
-nodes[0][0].total = 0
-nodes[0][0].risk = 0
-
-Q.put(nodes[0][0])
-
-nr_visited = 0
-while nr_visited < xmax*ymax:
-    cur = Q.get()
-    nr_visited += 1
-    #print(f'{cur.pos}, R:{cur.risk} T:{cur.total}')
-    
-    for n in neighbors(cur, nodes):
-        if not n.visited and cur.total + n.risk < n.total:
-            n.total = cur.total + n.risk
-            n.prev = cur
-            Q.put(n)
-
-#print(nodes)
-n = nodes[ymax-1][xmax-1]
-#print(f'{n.pos}, {n.risk} {n.total}')
-
-s = 0
-while True:
-    print(f'{n.pos}, R:{n.risk} T:{n.total}')
-    s += n.risk
-    if not n.prev:
-        break
-    n = n.prev
-print(s)
-# n = nodes[ymax-1][xmax-1]
-# n.prev = None
-# print(n)
+    even_more_risks = []
+    for a in range(5):
+        for row in more_risks:
+            even_more_risks.append(list(map(lambda v: incwrap(v, a), row)))
+    return even_more_risks
 
 
+test = """
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+"""
+
+# data = test
+with open('input', 'r') as f:
+    data = f.read()
+
+risks = [[int(c) for c in line] for line in data.strip().split()]
+
+print(f'Part 1: {dijkstra_eller_nåt(risks)}')
+
+many_risks = wow_such_risk(risks)
+print(f'Part 2: {dijkstra_eller_nåt(many_risks)}')
